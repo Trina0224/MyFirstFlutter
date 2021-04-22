778210cae70bae3aa21c1158a8f3315ebf153c8a
Trina0224 <trina.shih@gmail.com>
2021-04-22 09:01:49 -0700

    video26

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      //1st Version, video 24
+      // home: Center(
+      //   child: Text('Hello World'),
+      // ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.pink[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://avatars.githubusercontent.com/u/5771864?v=4'),
+          ),
+        ),
       ),
     ),
   );
